4e56622c6802f323278f7707527c59f8abd46ee9
mosixmus <spdt101mos@gmail.com>
2025-11-06 20:46:49 +0700

    fixed sidebar full height

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -41,7 +41,7 @@ const Sidebar = () => {
                 onClick={() => setOpen(false)}
             />
         )}
-        <nav className={`fixed md:static inset-y-0 left-0 z-50 shadow-md h-screen p-3 flex flex-col duration-500 ${open ? 'w-60 md:w-75' : 'w-20 md:w-30'} bg-blue-900 text-white`} >
+        <nav className={`fixed md:static inset-y-0 left-0 z-50 shadow-md min-h-screen h-auto p-3 flex flex-col duration-500 ${open ? 'w-60 md:w-75' : 'w-20 md:w-30'} bg-blue-900 text-white`} >
             {/* Header */}
             <div className={`px-3 py-2 h-25 flex items-center ${open ? 'justify-between' : 'justify-center'}`}>
                 <Image src={logo} alt="Logo" className={`${open ? 'w-10' : 'w-0'} rounded-md`} />
