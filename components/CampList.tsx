069226696908b8548eba42fba789044ba21edb3d
mosixmus <spdt101mos@gmail.com>
2025-12-02 10:02:20 +0700

    first added edit camp details (just front)

--- components/CampList.tsx
+++ components/CampList.tsx
@@ -50,6 +50,24 @@ interface AddModalProps {
   onSubmit: (e: React.FormEvent) => void;
 }
 
+interface EditModalProps {
+  isEditModalOpen: boolean;
+  onClose: () => void;
+  campData: {
+    title: string;
+    class: any;
+    date: string;
+    max: number;
+  }
+  setCampData: React.Dispatch<React.SetStateAction<{
+    title: string;
+    class: any;
+    date: string;
+    max: number;
+  }>>;
+  onSubmit: (e: React.FormEvent) => void;
+}
+
 const AddModal = ({ isOpen, onClose, formData, setFormData, onSubmit }: AddModalProps) => {
   if (!isOpen) return null;
 
@@ -99,20 +117,6 @@ const AddModal = ({ isOpen, onClose, formData, setFormData, onSubmit }: AddModal
 
           <div>
             <label className="block text-sm font-medium mb-1">ห้อง</label>
-            {/* <select
-              name="class"
-              value={formData.class}
-              onChange={(e) =>
-                setFormData({ ...formData, class: Number(e.target.value) })
-              }
-              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              required
-            >
-              <option value="">เลือกห้อง</option>
-              <option value="409">4/9</option>
-              <option value="509">5/9</option>
-              <option value="609">6/9</option>
-            </select> */}
 
             <Select options={options} value={options.find(o => o.value === formData.class)} 
               onChange={(selectedOption) =>
@@ -172,10 +176,116 @@ const AddModal = ({ isOpen, onClose, formData, setFormData, onSubmit }: AddModal
   )
 }
 
+const EditModal = ({ isEditModalOpen, onClose, campData, setCampData, onSubmit }: EditModalProps) => {
+  if (!isEditModalOpen) return null;
+  
+  const handleSave = async () => {
+
+  }
+
+  return (
+     <div
+      className="fixed inset-0 flex items-center justify-center z-50 p-5"
+      style={{ backgroundColor: "rgba(0, 0, 0, 0.8)" }}
+      onClick={onClose} // ปิด modal เมื่อคลิกพื้นหลัง
+    >
+      <div
+        className="bg-white px-10 py-6 rounded-lg w-full max-w-md"
+        onClick={(e) => e.stopPropagation()} // ป้องกันการปิด modal เมื่อคลิกภายใน
+      >
+        <h2
+          className="text-3xl font-semibold mb-4"
+          style={{ fontFamily: 'Mitr, sans-serif' }}
+        >
+          เพิ่มค่าย & กิจกรรม
+        </h2>
+
+        <form className="space-y-4" onSubmit={handleSave}>
+          <div>
+            <label className="block text-sm font-medium mb-1">ชื่อกิจกรรม</label>
+            <input
+              type="text"
+              name="title"
+              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="กรอกชื่อกิจกรรม"
+              value={campData.title}
+              onChange={(e) => {
+                setCampData({ ...campData, title: e.target.value });
+              }}
+              required
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium mb-1">ห้อง</label>
+            <Select options={options} value={options.find(o => o.value === campData.class)} 
+              onChange={(selectedOption) =>
+                setCampData({ ...campData, class: selectedOption ? selectedOption.value : null })
+              } />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium mb-1">วันที่</label>
+            <input
+              type="date"
+              name="date"
+              value={campData.date}
+              onChange={(e) =>
+                setCampData({ ...campData, date: e.target.value })
+              }
+              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              required
+            />
+          </div>
+
+          <div>
+            <label className="block text-sm font-medium mb-1">จำนวนคนต่อห้อง</label>
+            <input
+              type="number"
+              name="max"
+              value={campData.max || ''}
+              onChange={(e) =>
+                setCampData({ ...campData, max: Number(e.target.value) })
+              }
+              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="กรอกจำนวนคน"
+              min="1"
+              required
+            />
+          </div>
+
+          <div className="flex gap-3 pt-4">
+            <button
+              type="button"
+              onClick={onClose}
+              className="flex-1 px-4 py-2 border rounded-md hover:bg-gray-100 transition-colors"
+            >
+              ยกเลิก
+            </button>
+            <button
+              type="submit"
+              className="flex-1 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition-colors"
+            >
+              บันทึก
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
 const CampList = ({ Camps }: CampListProps) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [filterClass, setFilterClass] = useState('all');
   const [formData, setFormData] = useState({
+    title: '',
+    class: Number(''),
+    date: '',
+    max: Number('')
+  })
+  const [campData, setCampData] = useState({
     title: '',
     class: Number(''),
     date: '',
@@ -184,11 +294,18 @@ const CampList = ({ Camps }: CampListProps) => {
 
   function onClose() {
     setIsModalOpen(false);
+    setIsEditModalOpen(false);
     setFormData({
       title: '',
       class: Number(''),
       date: '',
       max: Number(''),
+    })
+    setCampData({
+      title: '',
+      class: Number(''),
+      date: '',
+      max: Number('')
     })
   }
 
@@ -250,6 +367,16 @@ const CampList = ({ Camps }: CampListProps) => {
       alert("เกิดข้อผิดพลาดในการลบข้อมูล");
     }
   }
+  
+  const updateCampDataFocus = async (camp: any) => {
+    setCampData({
+      title: camp.title,
+      class: camp.class,
+      date: camp.date,
+      max: camp.max
+    })
+    setIsEditModalOpen(true);
+  }
 
   const filteredCamps = filterClass === 'all'
     ? Camps
@@ -294,12 +421,20 @@ const CampList = ({ Camps }: CampListProps) => {
                 <td className="border p-2 md:p-3 text-center">{formatDate(item.date)}</td>
                 <td className="border p-2 md:p-3 text-center">{item.max}</td>
                 <td className="md:p-3 p-2 text-center">
-                  <button
-                    onClick={() => handleDelete(item.id)}
-                    className="px-2 py-1 md:py-1.5 md:px-3 text-sm md:text-base bg-red-500 text-white rounded hover:bg-red-600 cursor-pointer"
-                  >
-                    ลบ
-                  </button>
+                  <div className="flex justify-center gap-2">
+                    <button
+                      onClick={() => handleDelete(item.id)}
+                      className="px-2 py-1 md:py-1.5 md:px-3 text-sm md:text-base bg-red-500 text-white rounded hover:bg-red-600 cursor-pointer"
+                    >
+                      ลบ
+                    </button>
+                    <button
+                      onClick={() => updateCampDataFocus(item)}
+                      className="px-2 py-1 md:py-1.5 md:px-3 text-sm md:text-base bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer"
+                    >
+                      แก้ไข
+                    </button>
+                  </div>
                 </td>
               </tr>
             ))}
@@ -319,6 +454,14 @@ const CampList = ({ Camps }: CampListProps) => {
         onClose={onClose}
         formData={formData}
         setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+
+      <EditModal
+        isEditModalOpen={isEditModalOpen}
+        onClose={onClose}
+        campData={campData}
+        setCampData={setCampData}
         onSubmit={onSubmit}
       />
     </>
